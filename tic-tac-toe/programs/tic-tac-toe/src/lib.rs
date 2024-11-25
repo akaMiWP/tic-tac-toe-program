7f7ad7f8c53a8b26cf6ce4643c3d86dec9ed0899
akaMiWP <bangkokappdevelopment@gmail.com>
2024-11-25 21:50:47 +0700

    Finished the project

--- tic-tac-toe/programs/tic-tac-toe/src/lib.rs
+++ tic-tac-toe/programs/tic-tac-toe/src/lib.rs
@@ -12,6 +12,18 @@ pub mod tic_tac_toe {
             .game
             .start([ctx.accounts.player_one.key(), player_two])
     }
+
+    pub fn play(ctx: Context<Play>, tile: Tile) -> Result<()> {
+        let game = &mut ctx.accounts.game;
+
+        require_keys_eq!(
+            game.current_player(),
+            ctx.accounts.player.key(),
+            TicTacToeError::NotPlayersTurn
+        );
+
+        game.play(&tile)
+    }
 }
 
 #[derive(Accounts)]
@@ -22,6 +34,13 @@ pub struct SetupGame<'info> {
     #[account(mut)]
     pub player_one: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Play<'info> {
+    #[account(mut)]
+    pub game: Account<'info, Game>,
+    pub player: Signer<'info>,
 }
 
 #[account]
--- tic-tac-toe/tests/tic-tac-toe.ts
+++ tic-tac-toe/tests/tic-tac-toe.ts
@@ -35,4 +35,91 @@ describe("tic-tac-toe", () => {
       [null, null, null],
     ]);
   });
+
+  it("player one wins", async () => {
+    const gameKeypair = anchor.web3.Keypair.generate();
+    const playerOne = (program.provider as anchor.AnchorProvider).wallet;
+    const playerTwo = anchor.web3.Keypair.generate();
+    await program.methods
+      .setupGame(playerTwo.publicKey)
+      .accounts({
+        game: gameKeypair.publicKey,
+        playerOne: playerOne.publicKey,
+      })
+      .signers([gameKeypair])
+      .rpc();
+
+    let gameState = await program.account.game.fetch(gameKeypair.publicKey);
+    expect(gameState.turn).to.equal(1);
+    expect(gameState.players).to.eql([
+      playerOne.publicKey,
+      playerTwo.publicKey,
+    ]);
+    expect(gameState.state).to.eql({ active: {} });
+    expect(gameState.board).to.eql([
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ]);
+
+    await play(
+      program,
+      gameKeypair.publicKey,
+      playerOne,
+      { row: 0, column: 0 },
+      2,
+      { active: {} },
+      [
+        [{ x: {} }, null, null],
+        [null, null, null],
+        [null, null, null],
+      ]
+    );
+
+    try {
+      await play(
+        program,
+        gameKeypair.publicKey,
+        playerTwo,
+        { row: 5, column: 1 }, // ERROR: out of bounds row
+        4,
+        { active: {} },
+        [
+          [{ x: {} }, { x: {} }, null],
+          [{ o: {} }, null, null],
+          [null, null, null],
+        ]
+      );
+      // we use this to make sure we definitely throw an error
+      chai.assert(false, "should've failed but didn't ");
+    } catch (_err) {
+      expect(_err).to.be.instanceOf(anchor.AnchorError);
+      const err: anchor.AnchorError = _err;
+      expect(err.error.errorCode.number).to.equal(6000);
+    }
+  });
 });
+
+async function play(
+  program: Program<TicTacToe>,
+  game,
+  player,
+  tile,
+  expectedTurn,
+  expectedGameState,
+  expectedBoard
+) {
+  await program.methods
+    .play(tile)
+    .accounts({
+      player: player.publicKey,
+      game,
+    })
+    .signers(player instanceof (anchor.Wallet as any) ? [] : [player])
+    .rpc();
+
+  const gameState = await program.account.game.fetch(game);
+  expect(gameState.turn).to.equal(expectedTurn);
+  expect(gameState.state).to.eql(expectedGameState);
+  expect(gameState.board).to.eql(expectedBoard);
+}
